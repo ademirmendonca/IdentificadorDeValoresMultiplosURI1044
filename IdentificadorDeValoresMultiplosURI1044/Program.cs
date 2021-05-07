0a8cc3ec901aa77b2f98805d714a33654835c445
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-05-07 16:32:56 -0300

    Add project files.

---
+++ IdentificadorDeValoresMultiplosURI1044/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IdentificadorDeValoresMultiplosURI1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Captura os dados e recorta a string
+            string[] vet = Console.ReadLine().Split(' ');
+
+            //Aponta aonde cada valor separado por espaço será armazenado
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);            
+
+            //Condição para identificação se os valores são multiplos ou não
+            if ((a % b == 0) || (b % a == 0))
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+        }
+    }
+}
